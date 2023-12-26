0f78dc6868cc0c770c92ff173674e0f5e7036d19
ahincho <ahincho@unsa.edu.pe>
2023-12-26 01:59:27 -0500

    Spring Boot Initial Project

---
+++ src/main/java/com/unsa/suppliers/SuppliersApplication.java
@@ -0,0 +1,13 @@
+package com.unsa.suppliers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unsa/suppliers/SuppliersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsa.suppliers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppliersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
