6b8ae3c61840d8084be5615cd27511c34e092bc5
ahincho <ahincho@unsa.edu.pe>
2023-12-27 12:51:17 -0500

    Fixing Extra Space

--- src/main/java/com/unsa/suppliers/application/services/UserDetailsServiceImpl.java
+++ src/main/java/com/unsa/suppliers/application/services/UserDetailsServiceImpl.java
@@ -20,7 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with the username " + username + "  does not exists"));
+        UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with the username " + username + " does not exists"));
         Collection<? extends GrantedAuthority> authorities = userEntity.getRoles().stream()
                 .map(roleEntity -> new SimpleGrantedAuthority("ROLE_" + roleEntity.getName())).collect(Collectors.toSet());
         return new User(userEntity.getUsername(), userEntity.getPassword(), true, true, true, true, authorities);
