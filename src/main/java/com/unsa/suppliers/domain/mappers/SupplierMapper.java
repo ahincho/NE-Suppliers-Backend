56273c331fa2d10d14316d69c2c84fac9ba96331
ahincho <ahincho@unsa.edu.pe>
2023-12-27 11:58:23 -0500

    Supplier Mapper

---
+++ src/main/java/com/unsa/suppliers/domain/mappers/SupplierMapper.java
@@ -0,0 +1,21 @@
+package com.unsa.suppliers.domain.mappers;
+
+import com.unsa.suppliers.domain.dtos.suppliers.SupplierRequest;
+import com.unsa.suppliers.domain.dtos.suppliers.SupplierResponse;
+import com.unsa.suppliers.domain.entities.SupplierEntity;
+
+public class SupplierMapper {
+    public SupplierResponse entityToResponse(SupplierEntity supplierEntity) {
+        return SupplierResponse.builder()
+                .id(supplierEntity.getId())
+                .name(supplierEntity.getName())
+                .stateId(supplierEntity.getState().getId())
+                .stateName(supplierEntity.getState().getName())
+                .build();
+    }
+    public SupplierEntity requestToEntity(SupplierRequest supplierRequest) {
+        return SupplierEntity.builder()
+                .name(supplierRequest.getName())
+                .build();
+    }
+}
