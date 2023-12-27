b9731b58f5f21485acb1515059d478b9e4ddbf15
ahincho <ahincho@unsa.edu.pe>
2023-12-27 12:24:20 -0500

    Rest Advice to State Exceptions

---
+++ src/main/java/com/unsa/suppliers/infrastructure/handlers/StateExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.unsa.suppliers.infrastructure.handlers;
+
+import com.unsa.suppliers.domain.exceptions.states.StateDuplicatedException;
+import com.unsa.suppliers.domain.exceptions.states.StateNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class StateExceptionHandler {
+    @ExceptionHandler(StateNotFoundException.class)
+    public ResponseEntity<Void> stateNotFound() {
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(StateDuplicatedException.class)
+    public ResponseEntity<Void> stateDuplicated() {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+}
