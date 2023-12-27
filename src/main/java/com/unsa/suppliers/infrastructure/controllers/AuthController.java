67d0ccf3ea75e4f1e01e1900e3a6a5c4034af2d9
ahincho <ahincho@unsa.edu.pe>
2023-12-27 02:06:56 -0500

    Auth Rest Controller

---
+++ src/main/java/com/unsa/suppliers/infrastructure/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package com.unsa.suppliers.infrastructure.controllers;
+
+import com.unsa.suppliers.application.services.UserService;
+import com.unsa.suppliers.domain.dtos.users.UserRequest;
+import com.unsa.suppliers.domain.dtos.users.UserResponse;
+import com.unsa.suppliers.domain.entities.UserEntity;
+import com.unsa.suppliers.domain.exceptions.users.UserDuplicatedEmailException;
+import com.unsa.suppliers.domain.mappers.UserMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserService userService;
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody @Valid UserRequest userRequest) throws UserDuplicatedEmailException, UserDuplicatedUsernameException, RoleNotFoundException {
+        UserEntity savedUserEntity = userService.createUser(UserMapper.requestToEntity(userRequest));
+        return ResponseEntity.ok(UserMapper.entityToResponse(savedUserEntity));
+    }
+}
