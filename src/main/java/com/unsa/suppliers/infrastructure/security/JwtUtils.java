0ddfae1f11a9fa559b9877d506d248c90e2bb6b1
ahincho <ahincho@unsa.edu.pe>
2023-12-27 01:56:57 -0500

    Json Web Token Utils to Code and Decode Info

---
+++ src/main/java/com/unsa/suppliers/infrastructure/security/JwtUtils.java
@@ -0,0 +1,58 @@
+package com.unsa.suppliers.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    @Value("${jwt.time.expiration}")
+    private String expirationTime;
+    public Key getSignatureKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String createAccessToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(expirationTime)))
+                .signWith(getSignatureKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignatureKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+    public Claims getAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public <T> T getClaim(String token, Function<Claims, T> claimsFunction) {
+        Claims claims = getAllClaims(token);
+        return claimsFunction.apply(claims);
+    }
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+}
