a1030b4e5f343028e5c0eb47b3ea45ba000404f2
ahincho <ahincho@unsa.edu.pe>
2023-12-27 02:22:18 -0500

    Role Based Access Control for Role Endpoint

--- src/main/java/com/unsa/suppliers/infrastructure/controllers/RoleController.java
+++ src/main/java/com/unsa/suppliers/infrastructure/controllers/RoleController.java
@@ -8,6 +8,7 @@ import com.unsa.suppliers.domain.exceptions.roles.RoleDuplicatedException;
 import com.unsa.suppliers.domain.exceptions.roles.RoleNotFoundException;
 import com.unsa.suppliers.domain.mappers.RoleMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import jakarta.validation.Valid;
@@ -22,28 +23,33 @@ public class RoleController {
         this.roleService = roleService;
     }
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<RoleResponse>> getAll() {
         List<RoleEntity> roleEntities = roleService.getAllRoles();
         if (roleEntities.isEmpty()) { return ResponseEntity.noContent().build(); }
         return ResponseEntity.ok(roleEntities.stream().map(RoleMapper::entityToResponse).toList());
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<RoleResponse> getById(@PathVariable("id") Integer id) throws RoleNotFoundException {
         RoleEntity roleEntity = roleService.getRoleById(id);
         return ResponseEntity.ok(RoleMapper.entityToResponse(roleEntity));
     }
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<RoleResponse> save(@RequestBody @Valid RoleRequest roleRequest, UriComponentsBuilder uriComponentsBuilder) throws RoleDuplicatedException {
         RoleEntity roleEntity = roleService.createRole(RoleMapper.requestToEntity(roleRequest));
         URI uri = uriComponentsBuilder.path("/api/roles/{id}").buildAndExpand(roleEntity.getId()).toUri();
         return ResponseEntity.created(uri).body(RoleMapper.entityToResponse(roleEntity));
     }
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> update(@PathVariable("id") Integer id, @RequestBody @Valid RoleRequest roleRequest) throws RoleNotFoundException, RoleDuplicatedException {
         roleService.updateRole(id, RoleMapper.requestToEntity(roleRequest));
         return ResponseEntity.notFound().build();
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) throws RoleNotFoundException {
         roleService.deleteRole(id);
         return ResponseEntity.notFound().build();
