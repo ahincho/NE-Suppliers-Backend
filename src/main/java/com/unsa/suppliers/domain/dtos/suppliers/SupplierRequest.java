8b102749620f7aa937eb1212b3bec3af2ebaa3ab
ahincho <ahincho@unsa.edu.pe>
2023-12-27 11:09:28 -0500

    Supplier Data Transfer Objects

---
+++ src/main/java/com/unsa/suppliers/domain/dtos/suppliers/SupplierRequest.java
@@ -0,0 +1,15 @@
+package com.unsa.suppliers.domain.dtos.suppliers;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupplierRequest {
+    @NotBlank(message = "Name must not be blank")
+    private Integer name;
+}
