f937b9cec08467e4dfd8dccaa99298cd1c4b39c0
ahincho <ahincho@unsa.edu.pe>
2023-12-30 17:49:22 -0500

    Rest Controller Advices for Category and Country

---
+++ src/main/java/com/unsa/suppliers/infrastructure/handlers/CountryExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.unsa.suppliers.infrastructure.handlers;
+
+import com.unsa.suppliers.domain.exceptions.countries.CountryNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CountryExceptionHandler {
+    @ExceptionHandler(CountryNotFoundException.class)
+    public ResponseEntity<Void> countryNotFound() {
+        return ResponseEntity.notFound().build();
+    }
+    public ResponseEntity<Void> countryDuplicated() {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+}
