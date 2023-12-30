d6210f25099ffd83cfa7ed3e43415345eb80c4c2
ahincho <ahincho@unsa.edu.pe>
2023-12-30 17:06:17 -0500

    Mapping Category and Country Id to an Entity

--- src/main/java/com/unsa/suppliers/application/mappers/SupplierMapper.java
+++ src/main/java/com/unsa/suppliers/application/mappers/SupplierMapper.java
@@ -1,11 +1,26 @@
-package com.unsa.suppliers.domain.mappers;
+package com.unsa.suppliers.application.mappers;
 
 import com.unsa.suppliers.domain.dtos.suppliers.SupplierRequest;
 import com.unsa.suppliers.domain.dtos.suppliers.SupplierResponse;
+import com.unsa.suppliers.domain.entities.CategoryEntity;
+import com.unsa.suppliers.domain.entities.CountryEntity;
 import com.unsa.suppliers.domain.entities.SupplierEntity;
+import com.unsa.suppliers.domain.exceptions.categories.CategoryNotFoundException;
+import com.unsa.suppliers.domain.exceptions.countries.CountryNotFoundException;
+import com.unsa.suppliers.domain.repositories.CategoryRepository;
+import com.unsa.suppliers.domain.repositories.CountryRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class SupplierMapper {
+    private final CategoryRepository categoryRepository;
+    private final CountryRepository countryRepository;
+    public SupplierMapper(CategoryRepository categoryRepository, CountryRepository countryRepository) {
+        this.categoryRepository = categoryRepository;
+        this.countryRepository = countryRepository;
+    }
     public SupplierResponse entityToResponse(SupplierEntity supplierEntity) {
         return SupplierResponse.builder()
                 .id(supplierEntity.getId())
@@ -18,11 +33,23 @@ public class SupplierMapper {
                 .stateName(supplierEntity.getState().getName())
                 .build();
     }
-    public SupplierEntity requestToEntity(SupplierRequest supplierRequest) {
+    public SupplierEntity requestToEntity(SupplierRequest supplierRequest) throws CategoryNotFoundException, CountryNotFoundException {
         return SupplierEntity.builder()
                 .name(supplierRequest.getName())
                 .ruc(supplierRequest.getRuc())
-                .country()
+                .category(findCategoryByIdOrThrowNotFound(supplierRequest.getCategoryId()))
+                .country(findCountryByIdOrThrowNotFound(supplierRequest.getCountryId()))
                 .build();
+    }
+    // Private Auxiliary Methods because Boiler Code
+    private CategoryEntity findCategoryByIdOrThrowNotFound(Integer id) throws CategoryNotFoundException {
+        Optional<CategoryEntity> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isEmpty()) { throw new CategoryNotFoundException(); }
+        return optionalCategory.get();
+    }
+    private CountryEntity findCountryByIdOrThrowNotFound(Integer id) throws CountryNotFoundException {
+        Optional<CountryEntity> optionalCountry = countryRepository.findById(id);
+        if (optionalCountry.isEmpty()) { throw new CountryNotFoundException(); }
+        return optionalCountry.get();
     }
 }
