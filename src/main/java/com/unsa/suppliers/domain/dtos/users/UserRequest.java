bae57282074689efebee800d9babe71e139260b3
ahincho <ahincho@unsa.edu.pe>
2023-12-31 23:12:57 -0500

    Update Default Messages

--- src/main/java/com/unsa/suppliers/domain/dtos/users/UserRequest.java
+++ src/main/java/com/unsa/suppliers/domain/dtos/users/UserRequest.java
@@ -12,19 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequest {
-    @NotBlank(message = "Name must not be blank")
+    @NotBlank(message = "Name field is required")
     @Size(min = 3, message = "Name must contain at least 3 characters")
     private String name;
-    @NotBlank(message = "Lastname must not be blank")
+    @NotBlank(message = "Lastname field is required")
     @Size(min = 3, message = "Lastname must contain at least 3 characters")
     private String lastname;
-    @NotBlank(message = "Username must not be blank")
+    @NotBlank(message = "Username field is required")
     @Size(min = 4, message = "Username must contain at least 4 characters")
     private String username;
-    @NotBlank(message = "Email must not be blank")
+    @NotBlank(message = "Email field is required")
     @Email(message = "Please send a valid email")
     private String email;
-    @NotBlank(message = "Password must not be blank")
+    @NotBlank(message = "Password field is required")
     @Size(min = 8, message = "Password must contain at least 8 characters")
     private String password;
 }
