cb85ea227efaae0c8b6c26ad03d9cedba490d260
ahincho <ahincho@unsa.edu.pe>
2024-01-04 19:28:34 -0500

    Role Mapper

--- src/main/java/com/unsa/suppliers/application/mappers/RoleMapper.java
+++ src/main/java/com/unsa/suppliers/application/mappers/RoleMapper.java
@@ -3,15 +3,17 @@ package com.unsa.suppliers.application.mappers;
 import com.unsa.suppliers.domain.dtos.roles.RoleRequest;
 import com.unsa.suppliers.domain.dtos.roles.RoleResponse;
 import com.unsa.suppliers.domain.entities.RoleEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RoleMapper {
-    public static RoleResponse entityToResponse(RoleEntity roleEntity) {
+    public RoleResponse entityToResponse(RoleEntity roleEntity) {
         return RoleResponse.builder()
                 .id(roleEntity.getId())
                 .name(roleEntity.getName())
                 .build();
     }
-    public static RoleEntity requestToEntity(RoleRequest roleRequest) {
+    public RoleEntity requestToEntity(RoleRequest roleRequest) {
         return RoleEntity.builder()
                 .name(roleRequest.getName())
                 .build();
