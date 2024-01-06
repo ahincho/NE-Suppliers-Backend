268d79fedf9d19e4d0a600904ef134c022f7a37e
ahincho <ahincho@unsa.edu.pe>
2024-01-06 12:10:35 -0500

    Category Service

---
+++ src/main/java/com/unsa/suppliers/application/services/CategoryService.java
@@ -0,0 +1,56 @@
+package com.unsa.suppliers.application.services;
+
+import static com.unsa.suppliers.application.services.StateService.ACTIVE_STATE;
+
+import com.unsa.suppliers.domain.entities.CategoryEntity;
+import com.unsa.suppliers.domain.entities.StateEntity;
+import com.unsa.suppliers.domain.exceptions.categories.CategoryDuplicatedException;
+import com.unsa.suppliers.domain.exceptions.categories.CategoryNotFoundException;
+import com.unsa.suppliers.domain.exceptions.states.StateNotFoundException;
+import com.unsa.suppliers.domain.repositories.CategoryRepository;
+import com.unsa.suppliers.domain.repositories.StateRepository;
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final StateRepository stateRepository;
+    public CategoryService(CategoryRepository categoryRepository, StateRepository stateRepository) {
+        this.categoryRepository = categoryRepository;
+        this.stateRepository = stateRepository;
+    }
+    public List<CategoryEntity> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+    public CategoryEntity getCategoryById(Integer id) throws CategoryNotFoundException {
+        return categoryRepository.findById(id).orElseThrow(CategoryNotFoundException::new);
+    }
+    @Transactional
+    public CategoryEntity createCategory(CategoryEntity categoryEntity) throws CategoryDuplicatedException, StateNotFoundException {
+        Optional<CategoryEntity> optionalCategory = categoryRepository.findByName(categoryEntity.getName());
+        if (optionalCategory.isPresent()) { throw new CategoryDuplicatedException(); }
+        StateEntity stateEntity = stateRepository.findByName(ACTIVE_STATE).orElseThrow(StateNotFoundException::new);
+        categoryEntity.setState(stateEntity);
+        return categoryRepository.save(categoryEntity);
+    }
+    @Transactional
+    public void updateCategory(Integer id, CategoryEntity categoryEntity) throws CategoryNotFoundException, CategoryDuplicatedException {
+        CategoryEntity optionalCategory = categoryRepository.findById(id).orElseThrow(CategoryNotFoundException::new);
+        if (!optionalCategory.getName().equals(categoryEntity.getName())) {
+            Optional<CategoryEntity> optionalCategoryName = categoryRepository.findByName(categoryEntity.getName());
+            if (optionalCategoryName.isPresent()) { throw new CategoryDuplicatedException(); }
+        }
+        categoryEntity.setId(id);
+        categoryRepository.save(categoryEntity);
+    }
+    @Transactional
+    public void changeCategoryState(Integer id, String state) throws CategoryNotFoundException, StateNotFoundException {
+        CategoryEntity categoryEntity = categoryRepository.findById(id).orElseThrow(CategoryNotFoundException::new);
+        StateEntity stateEntity = stateRepository.findByName(state).orElseThrow(StateNotFoundException::new);
+        categoryEntity.setState(stateEntity);
+        categoryRepository.save(categoryEntity);
+    }
+}
