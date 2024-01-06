9765886ca8c5f299c64188e8ea26c4a5817d3705
ahincho <ahincho@unsa.edu.pe>
2024-01-06 13:50:33 -0500

    State In Use Exception

--- src/main/java/com/unsa/suppliers/domain/exceptions/states/StateExceptionMessages.java
+++ src/main/java/com/unsa/suppliers/domain/exceptions/states/StateExceptionMessages.java
@@ -3,4 +3,5 @@ package com.unsa.suppliers.domain.exceptions.states;
 public class StateExceptionMessages {
     public static final String DUPLICATED = "This state is already register in the database";
     public static final String NOT_FOUND = "There is no state with the requested identifier";
+    public static final String IN_USE = "This state is used on other tables";
 }
