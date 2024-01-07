5e8aeba9dc2310a5c84757fd282f63cd7bd1d211
ahincho <ahincho@unsa.edu.pe>
2024-01-06 20:30:26 -0500

    State Request and Response

---
+++ src/main/java/com/unsa/suppliers/domain/dtos/states/StateResponse.java
@@ -0,0 +1,14 @@
+package com.unsa.suppliers.domain.dtos.states;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StateResponse {
+    private Integer id;
+    private String name;
+}
